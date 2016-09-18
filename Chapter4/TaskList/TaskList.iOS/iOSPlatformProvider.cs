709ec1cb12345c66bb40b8847153fd99d28e9612
Adrian Hall <adrian@shellmonger.com>
2016-09-18 11:50:11 -0700

    Fix for iOS project

--- Chapter4/TaskList/TaskList.iOS/iOSPlatformProvider.cs
+++
@@ -1,9 +0,0 @@
-using TaskList.Abstractions;
-
-[assembly: Xamarin.Forms.Dependency(typeof(TaskList.iOS.iOSPlatformProvider))]
-namespace TaskList.iOS
-{
-    public class iOSPlatformProvider : IPlatformProvider
-    {
-    }
-}
