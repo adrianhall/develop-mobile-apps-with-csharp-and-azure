26ccdc9ae5ad0d07eb5157c035c80f9dd45d872c
Adrian Hall <adrian@shellmonger.com>
2017-01-04 22:54:47 -0800

    Added base chapter7 project

---
+++ Chapter7/VideoSearch/VideoSearch/ViewModels/Search.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VideoSearch.ViewModels
+{
+    public class Search : ViewModels.Base
+    {
+        public Search()
+        {
+            Title = "Video Search";
+        }
+
+        #region SearchString Property
+        private string _pSearchString;
+        public string SearchString
+        {
+            get { return _pSearchString; }
+            set { SetProperty(ref _pSearchString, value, "SearchString"); }
+        }
+        #endregion
+
+        #region Search Command
+        Command _cmdSearch;
+
+        public Command SearchCommand => _cmdSearch ?? (_cmdSearch = new Command(async () => await ExecuteSearchCommand()));
+
+        private async Task ExecuteSearchCommand()
+        {
+            if (IsBusy)
+                return;
+            IsBusy = true;
+
+            try
+            {
+                // Do Work here
+            }
+            catch (Exception ex)
+            {
+                // Do exception handling here
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Chapter7/VideoSearch/VideoSearch/Views/Search.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace VideoSearch.Views
+{
+    public partial class Search : ContentPage
+    {
+        public Search()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModels.Search();
+        }
+    }
+}
