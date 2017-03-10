cd6c0c81865a3ec895497a234ca70d3bb0ac22c6
Adrian Hall <adrian@shellmonger.com>
2017-03-09 21:29:08 -0800

    Fixed Chapter1 code

---
+++ Chapter1/Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Backend.Startup))]
+
+namespace Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
---
+++ Chapter1/TodoList/TodoList/Abstractions/ICloudService.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoList.Abstractions
+{
+   public interface ICloudService
+    {
+        ICloudTable<T> GetTable<T>() where T : TableData;
+    }
+}
