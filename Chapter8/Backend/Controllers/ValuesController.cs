b06bc02b15a697ceb65b6ee49a2c831f19b7b018
Adrian Hall <adrian@shellmonger.com>
2017-03-27 15:08:02 -0700

    Added Chapter8 base project

---
+++ Chapter8/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace Backend.Controllers
+{
+    // Use the MobileAppController attribute for each ApiController you want to use  
+    // from your mobile clients 
+    [MobileAppController]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+        // POST api/values
+        public string Post()
+        {
+            return "Hello World!";
+        }
+    }
+}
