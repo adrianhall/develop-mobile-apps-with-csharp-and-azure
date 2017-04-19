9245b847dc564ebfa501f6315c2485e3b5cf78f8
Adrian Hall <adrian@shellmonger.com>
2017-04-18 18:41:07 -0700

    Switch to WebView - maybe that works

--- Chapter7M/VideoApp/VideoApp/Pages/VideoDetail.xaml.cs
+++ Chapter7M/VideoApp/VideoApp/Pages/VideoDetail.xaml.cs
@@ -12,6 +12,38 @@ namespace VideoApp.Pages
 		public VideoDetail (Models.Video video)
 		{
 			InitializeComponent ();
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = @"
+<html>
+    <head>
+        <title>Test</title>
+        <link href=""https://amp.azure.net/libs/amp/1.8.3/skins/amp-default/azuremediaplayer.min.css"" rel=""stylesheet"">
+        <script src=""https://amp.azure.net/libs/amp/1.8.3/azuremediaplayer.min.js""></script>
+
+     </head>
+    <body>
+        <video id=""azuremediaplayer"" class=""azuremediaplayer amp-default-skin amp-big-play-centered"" tabindex=""0""></video>
+        <script>
+var myOptions = {
+	""nativeControlsForTouch"": false,
+    controls: true,
+	autoplay: true,
+	width: ""640"",
+	height: ""400"",
+};
+myPlayer = amp(""azuremediaplayer"", myOptions);
+myPlayer.src([
+    {
+    src: ""http://zumobook.streaming.mediaservices.windows.net/b45bc4e8-4a78-4691-9cf3-cf6a10ba36d5/toystory.ism/manifest"",
+    type: ""application/vnd.ms-sstr+xml""
+    }
+]);
+        </script>
+    </body>
+</html>
+";
+            browser.Source = htmlSource;
 		}
 	}
 }
